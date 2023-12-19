db939012171c20facb7550d60a4d72269076f204
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-19 11:46:10 +0100

    fatto metodo somaaArray

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quanti numeri voui sommare?: ");
+        int dimensione= scanner.nextInt();
+        scanner.nextLine();
+
+
+        int[] arr = new int[dimensione];
+
+        for(int i=0; i < dimensione; i++){
+            System.out.print((i + 1) + " Numero: " + "\n");
+            int numeroUtente = scanner.nextInt();
+            arr[i] = numeroUtente;
+        }
+
+        System.out.println("La somma :  " + sommaAray(arr));
+
+        scanner.close();
     }
+
+    public static int sommaAray (int[] arr){
+        int somma = 0;
+        for(int i=0; i < arr.length; i++){
+            somma += arr[i];
+        }
+
+        return somma;
+    }
+
+   
 }

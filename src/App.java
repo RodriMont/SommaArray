8221e86acb4a38bc295c39a4c202165f443de7aa
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-19 11:37:01 +0100

    secondo commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quanti numeri voui sommare?: ");
+        int dimensione= scanner.nextInt();
+        scanner.nextLine();
+
+
+        int[] arr = new int[dimensione];
+
+        for(int i=0; i < dimensione; i++){
+            System.out.print((i + 1) + " Numero: " + "\n");
+            int numeroUtente = scanner.nextInt();
+            arr[i] = numeroUtente;
+        }
+
+            
+      
+
+        
+        scanner.close();
     }
+
+   
 }
